2d9ead91ec461c06391b70bb94822cfbc49db2af
miyuki-kuwabara <kuwabara@mbh.nifty.com>
2021-04-03 20:57:58 +0900

    水文水質データベースの水位流量の解析もヘッダを解析するように修正

--- application/models/WaterLevelSources/Mlitt/LevelDataParser.php
+++ application/models/WaterLevelSources/Mlitt/LevelDataParser.php
@@ -1,26 +1,85 @@
 <?php
 namespace WaterLevelSources\Mlitt {
+    require_once APPPATH.'models/Entities/MeasuredValueTypes.php';
     require_once APPPATH.'models/WaterLevelSources/Mlitt/IDataParser.php';
+    require_once APPPATH.'models/WaterLevelSources/Mlitt/MeasuredValuesParser.php';
     
     class LevelDataParser implements IDataParser {
-        public function parse($content, $acquired_date) {
-            $data = array();
+        private $columns = array(
+            '日付' => array(
+                'key' => 'measured_at_date',
+                'parser' => 'parse_date'
+            ),
+            '時刻' => array(
+                'key' => 'measured_at_time',
+                'parser' => 'parse_time'
+            ),
+            'データ' => array(
+                'key' => 'value',
+                'parser' => 'parse_numeric'
+            ),
+            'フラグ' => array(
+                'key' => 'flags',
+                'parser' => 'parse_flags'
+            ),
+        );
+
+        public function parse($content, $acquired_at) {
+            $datum = array();
+            $indexes = null;
+            $headers = array_keys($this->columns);
+            $first_header = $headers[0];
             foreach (explode("\r\n", $content) as $line) {
                 $columns = explode(',', $line);
-                if (!preg_match('/^(\d{4})\/(\d{1,2})\/(\d{1,2})$/', $columns[0], $date))
-                    continue;
-                if (!checkdate($date[2] - 0, $date[3] - 0, $date[1] - 0))
-                    continue;
-                if (!preg_match('/^\d{2}:\d{2}$/', $columns[1]))
-                    continue;
-                $level = is_numeric($columns[2]) ? $columns[2] - 0 : null;
-                $data[] = array(
-                    'date' => "{$columns[0]} {$columns[1]}",
-                    'level' => $level,
-                    'acquired' => $acquired_date, 
-                );
+
+                if (mb_substr($columns[0], 0, 1) == '#') {
+                    $columns[0] = mb_substr($columns[0], 1);
+                    if ($columns[0] === $first_header) {
+                        $indexes = $this->extract_headers($columns);
+                    }
+                } elseif (isset($indexes)) {
+                    $result = $this->extract_data($indexes, $columns, $acquired_at);
+                    if (!empty($result)) $datum[] = $result;
+
+                } else;
             }
-            return $data;
+            return $datum;
+        }
+
+        private function extract_headers($headers) {
+            $columns = $this->columns;
+            return array_reduce(
+                $headers, 
+                function ($dictionary, $header) use ($columns) {
+                    $dictionary[] = array_key_exists($header, $columns)
+                        ? $columns[$header]
+                        : null;
+                    return $dictionary;
+                }, array());
+        }
+
+        private function extract_data($indexes, $columns, $acquired_at) {
+            $extracted = array();
+            $parser = new MeasuredValuesParser();
+            
+            foreach ($indexes as $i => $index) {
+                if (isset($index) && isset($columns[$i])) {
+                    $column = $columns[$i];
+                    $extracted[$index['key']] = $parser->$index['parser']($column);
+                }
+            }
+            
+            if (!isset($extracted['measured_at_date']) || !isset($extracted['measured_at_time'])) {
+                return null;
+            }
+
+            return  array(
+                'measured_at' => "{$extracted['measured_at_date']} {$extracted['measured_at_time']}",
+                'value_type' => \Entities\MeasuredValueTypes::WATER_LEVEL,
+                'value' => $extracted['value'],
+                'flags' => $extracted['flags'],
+                'acquired_at' => $acquired_at,
+            );
         }
     }    
 }
