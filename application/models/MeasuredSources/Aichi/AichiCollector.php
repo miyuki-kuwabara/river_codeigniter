a5b3ac033a9ceda542b9ff7eb98ba532f66404e9
miyuki-kuwabara <kuwabara@mbh.nifty.com>
2021-04-06 22:16:54 +0900

    愛知県 川の防災情報水位からのデータ抽出を実装

---
+++ application/models/MeasuredSources/Aichi/AichiCollector.php
@@ -0,0 +1,99 @@
+<?php
+namespace MeasuredSources\Aichi {
+    defined('BASEPATH') or exit('No direct script access allowed');
+    
+    require_once APPPATH.'models/Entities/MeasuredValueFlags.php';
+    require_once APPPATH.'models/Entities/MeasuredValueTypes.php';
+    require_once APPPATH.'models/HttpGetter.php';
+    require_once APPPATH.'models/HttpHeaderParser.php';
+    require_once APPPATH.'models/HttpEntitiySpaceReplacer.php';
+    require_once APPPATH.'models/MeasuredSources/MeasuredDateNormalizer.php';
+    require_once APPPATH.'models/MeasuredSources/IMeasuredSourceCollector.php';
+
+    class AichiCollector implements \MeasuredSources\IMeasuredSourceCollector
+    {
+        const MARKER_EXTRACT_START = '12時間履歴';
+        const MARKER_EXTRACT_END = '1時間履歴';
+
+        private $source_url = null;
+        private $entity_space_replacer = null;
+        private $measured_date_normalizer = null;
+
+        public function __construct($db, $source_url)
+        {
+            $this->source_url = $source_url;
+            $this->entity_space_replacer = new \HttpEntitiySpaceReplacer();
+            $this->measured_date_normalizer = new \MeasuredSources\MeasuredDateNormalizer();
+        }
+
+        public function get()
+        {
+            $getter = new \HttpGetter();
+            $data = $this->get_level_data($getter);
+            return $data;
+        }
+
+        private function get_level_data(\HttpGetter $getter)
+        {
+            $response = $getter->get($this->source_url);
+            $date = date('Y-m-d H:i:s');
+
+            libxml_use_internal_errors(true);
+            
+            $document = new \DOMDocument();
+            $load = $document->loadHTML($response);
+            if ($load === false) {
+                return null;
+            }
+
+            $bodies = $document->getElementsByTagName('body');
+            foreach ($bodies as $body) {
+                $content = $body->  textContent;
+                $result = $this->extract(
+                    $this->entity_space_replacer->replace($content),
+                    $date);
+                if (!empty($result)) {
+                    return $result;
+                }
+            }
+            return array();
+        }
+
+        private function extract($content, $acquired_at)
+        {
+            $datum = array();
+            $take = false;
+            foreach (explode("\n", $content) as $line) {
+                $line = trim($line);
+                if (!$take) {
+                    if (self::MARKER_EXTRACT_START === $line) {
+                        $take = true;
+                    }
+                } else {
+                    if (self::MARKER_EXTRACT_END === $line) {
+                        break;
+                    }
+
+                    if (preg_match('/(\d{1,2}:\d{2})\s+(\d+(?:\.\d+)?)/', $line, $matches)) {
+                        $measured_at = $this->measured_date_normalizer->normalize_time($matches[1]);
+                        if ($measured_at === null) {
+                            continue;
+                        }
+
+                        $value = is_numeric($matches[2]) ? $matches[2] - 0 : null;
+                        $datum[] = array(
+                            'measured_at' => $measured_at,
+                            'value_type' => \Entities\MeasuredValueTypes::WATER_LEVEL,
+                            'value' => $value,
+                            'flags' => isset($value)
+                                ? \Entities\MeasuredValueFlags::NONE
+                                : \Entities\MeasuredValueFlags::MISSED,
+                            'acquired_at' => $acquired_at,
+                        );
+                    }
+                }
+            }
+            return $datum;
+        }
+    }
+}
--- application/models/MeasuredSources/MeasuredSourceFactory.php
+++ application/models/MeasuredSources/MeasuredSourceFactory.php
@@ -1,43 +1,58 @@
 <?php
 namespace MeasuredSources {
-    defined('BASEPATH') OR exit('No direct script access allowed');
+    defined('BASEPATH') or exit('No direct script access allowed');
     require_once APPPATH.'models/MeasuredSources/Mlitt/MlittCollector.php';
     require_once APPPATH.'models/MeasuredSources/Araizeki/AraizekiCollector.php';
     require_once APPPATH.'models/MeasuredSources/Wakayama/WakayamaCollector.php';
     require_once APPPATH.'models/MeasuredSources/Nara/NaraCollector.php';
     require_once APPPATH.'models/MeasuredSources/Gifu/GifuCollector.php';
+    require_once APPPATH.'models/MeasuredSources/Aichi/AichiCollector.php';
 
-    class MeasuredSourceFactory {
-        public static function create_mlitt($db) {
+    class MeasuredSourceFactory
+    {
+        public static function create_mlitt($db)
+        {
             return Mlitt\MlittCollector::create_level($db, "http://www1.river.go.jp/cgi-bin/DspWaterData.exe?KIND=9&ID=306021286614020");
         }
 
-        public static function create_mlitt_dam($db) {
+        public static function create_mlitt_dam($db)
+        {
             return Mlitt\MlittCollector::create_dam($db, 'http://www1.river.go.jp/cgi-bin/DspDamData.exe?ID=1368051260060&KIND=3');
         }
 
-        public static function create_araizeki($db) {
+        public static function create_araizeki($db)
+        {
             return new Araizeki\AraizekiCollector($db, "https://www.kkr.mlit.go.jp/biwako/index.php");
         }
 
-        public static function create_wakayama_level($db) {
+        public static function create_wakayama_level($db)
+        {
             return Wakayama\WakayamaCollector::create_level($db, 'http://kasensabo02.pref.wakayama.lg.jp/keitai/suii/301406.html');
         }
 
-        public static function create_wakayama_dam_inflow($db) {
+        public static function create_wakayama_dam_inflow($db)
+        {
             return Wakayama\WakayamaCollector::create_dam_inflow($db, 'http://kasensabo02.pref.wakayama.lg.jp/keitai/dam/501-0.html');
         }
 
-        public static function create_wakayama_dam_outflow($db) {
+        public static function create_wakayama_dam_outflow($db)
+        {
             return Wakayama\WakayamaCollector::create_dam_outflow($db, 'http://kasensabo02.pref.wakayama.lg.jp/keitai/dam/501-1.html');
         }
 
-        public static function create_nara($db) {
+        public static function create_nara($db)
+        {
             return new Nara\NaraCollector($db, 'http://www.kasen.pref.nara.jp/sppub/status/river_log_1_131.html');
         }
 
-        public static function create_gifu($db) {
+        public static function create_gifu($db)
+        {
             return new Gifu\GifuCollector($db, 'http://www.kasen.pref.gifu.lg.jp/h/Valley_6_382.html');
+        }
+        
+        public static function create_aichi($db)
+        {
+            return new Aichi\AichiCollector($db, 'https://www.kasen-aichi.jp/fp/WaterLevelHis_6130.html');
         }
     }
 }
