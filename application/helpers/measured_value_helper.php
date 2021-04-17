23260913d26422203d872584323c3ba6fef012d5
miyuki-kuwabara <kuwabara@mbh.nifty.com>
2021-04-18 00:23:34 +0900

    measured_vaule_helperを追加。測定値周りの処理を統合する。

---
+++ application/helpers/measured_value_helper.php
@@ -0,0 +1,33 @@
+
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+require_once APPPATH.'models/Entities/MeasuredValueFlags.php';
+
+if (!function_exists('is_measured_value_enable')) {
+    function is_measured_value_enable($value, $flags)
+    {
+        return $value !== null &&
+            ($flags == \Entities\MeasuredValueFLags::NONE || $flags != \Entities\MeasuredValueFLags::TEMPORARY);
+    }
+}
+
+if (!function_exists('format_measured_value')) {
+    function format_measured_value($value, $flags, $decimal = 2)
+    {
+        if (is_measured_value_enable($value, $flags)) {
+            return $decimal == 0
+                ? sprintf('%d', $value)
+                : sprintf("%.{$decimal}f", $value);
+        }
+        return $decimal == 0
+            ? '--'
+            : '--.' . str_repeat('-', $decimal);
+    }
+}
+
+if (!function_exists('measured_value')) {
+    function measured_value($value, $flags, $decimal = 2)
+    {
+        echo format_measured_value($value, $flags, $decimal);
+    }
+}
