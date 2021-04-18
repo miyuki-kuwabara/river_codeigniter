0acc82d16fab2a783d7d0d4822a7285fd3521db8
miyuki-kuwabara <kuwabara@mbh.nifty.com>
2021-04-18 23:42:02 +0900

    ソースごとのビューについて、測定値の表示を測定日時で降順に変更

--- application/views/values/single_value.php
+++ application/views/values/single_value.php
@@ -2,6 +2,15 @@
 $value_key = "{$value_type}_value";
 $flags_key = "{$value_type}_flags";
 $unit_varname = "{$value_type}_unit";
+
+// 最新の有効なデータ
+$latest = null;
+foreach ($measured_data as $row) {
+    if (is_measured_value_enable($row[$value_key], $row[$flags_key])) {
+        $latest = $row;
+        break;
+    }
+}
 ?>
 <h1><?php eh($measure_source_name); ?>(<?php eh($value_type_name); ?>)</h1>
 <hr/>
@@ -14,21 +23,49 @@ $unit_varname = "{$value_type}_unit";
 <hr/>
 <table>
     <tbody>
-<?php $prev_date = null; ?>
-<?php foreach ($measured_data as $row):
-    $difference = 0;
-    if (is_measured_value_enable($row[$value_key], $row[$flags_key])) {
-        if (is_measured_value_enable($measured_data_last[$value_key], $measured_data_last[$flags_key])) {
-            $difference = $row[$value_key] - $measured_data_last[$value_key];
-        }
-        $measured_data_last = $row;
-    } ?>
+<?php
+if (!empty($measured_data)) {
+    // センチネルの追加
+    if ($measure_source_type == \Entities\MeasuredSourceTypes::ARAIZEKI) {
+        // 南郷洗堰については最古のデータも表示対象にするため、センチネルを別途追加する
+        $measured_data[] = array_reduce(
+            array_keys($measured_data[0]),
+            function ($a, $key) {
+                $a[$key] = null;
+            }, array('end' => 1));
+    } else {
+        // それ以外の場合は、最古のデータは次データに増減を表示するための捨てデータとなるため、センチネルのフラグのみ立てる
+        $measured_data[count($measured_data) - 1]['end'] = 1;
+    }
+}
+
+$prev_date = null;
+$output = array();
+// 測定日時順で降順に表示するが、過去データとの増減を出力する必要がある。、
+// このため、過去データに向かって遡り、有効な測定値か最古のデータが出てきたら
+// そこまでの測定値を出力する。
+foreach ($measured_data as $row) :
+    $last_enable = null;
+    if (is_measured_value_enable($row[$value_key], $row[$flags_key])):
+        $last_enable = $row[$value_key];
+    endif;
+    if (isset($row['end']) || isset($last_enable)):
+        foreach ($output as $current):
+            $difference = 0;
+            if (isset($last_enable) &&
+                is_measured_value_enable($current[$value_key], $current[$flags_key])):
+                $difference = $current[$value_key] - $last_enable;
+            endif; ?>
         <tr>
-            <td><?php if ($prev_date !== $row['measured_date']): eh($row['measured_date']); $prev_date = $row['measured_date']; endif; ?></td>
-            <td><?php eh($row['measured_time']); ?></td>
-            <td><?php if (0 < $difference): ?><span class="increase"><?php endif; measured_value($row[$value_key], $row[$flags_key]); if (0 < $difference): ?></span><?php endif; ?></td>
+            <td><?php if ($prev_date !== $current['measured_date']): eh($current['measured_date']); $prev_date = $current['measured_date']; endif; ?></td>
+            <td><?php eh($current['measured_time']); ?></td>
+            <td><?php if (0 < $difference): ?><span class="increase"><?php endif; measured_value($current[$value_key], $current[$flags_key]); if (0 < $difference): ?></span><?php endif; ?></td>
             <td><?php if ($difference < 0): ?>↓<?php elseif (0 < $difference): ?><span class="increse">↑</span><?php else: ?>→<?php endif; ?></td>
         </tr>
-<?php endforeach; ?>
+<?php   endforeach;
+        $output = array();
+    endif;
+    $output[] = $row;
+endforeach; ?>
     </tbody>
 </table>
